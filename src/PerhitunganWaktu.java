a84ba9f730ba512d6625e008e696fce2cb10e0d5
ilman nafian <ilmannafian587@gmail.com>
2023-11-01 12:58:33 +0700

    nambahkan file

---
+++ src/PerhitunganWaktu.java
@@ -0,0 +1,26 @@
+
+public class PerhitunganWaktu {
+    public static void main(String[] args) {
+
+        // Jarak dalam kilometer
+        double jarak= 100;
+
+
+        // Kecepatan rata-rata dalam kilometer per jam
+
+
+        double kecepatan = 50;
+/**
+ * Rumus mencari waktu
+ */
+        // Menghitung waktu dengan rumus jarak dibagi kecepatan
+        double waktu = jarak / kecepatan;
+
+        System.out.println("Jarak (km): " + jarak);
+        System.out.println("Kecepatan (km/jam): " + kecepatan);
+        System.out.println("Waktu (jam): " + waktu);
+
+    }
+}
+
+
